75303e92cdcd1cf78b3f41cfddc1c3b9435db2a0
Akrity3 <163756385+Akrity3@users.noreply.github.com>
2025-01-15 09:57:10 +0545

    making enum class to know the direction of the pipes.

---
+++ src/main/java/engine/Direction.java
@@ -0,0 +1,10 @@
+package engine;
+
+// This enum class is used for pipes in mario. Where there are pipes facing up, down, left and right;
+
+public enum Direction {
+    Up,
+    Down,
+    Left,
+    Right
+}
