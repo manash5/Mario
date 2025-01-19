eb8d1ec885eb6fdabf51429f591d29145154e539
sujita <sujitagopali@gmail.com>
2025-01-19 18:50:02 +0545

    Making this class only so that gizmos cannot be called as active object when clicked.

---
+++ src/main/java/components/NonPickable.java
@@ -0,0 +1,6 @@
+package components;
+
+// this class is made only so that gizmos cannot be called as active object when clicked
+// we didn't made gizmos an active object because when it is, it just flies away since it is constantly rendering itself
+public class NonPickable extends Component{
+}
