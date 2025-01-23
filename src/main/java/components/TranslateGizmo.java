63e4e7af8482e7d5d37c0c69bb105c03b560a5c8
sujita <sujitagopali@gmail.com>
2025-01-23 12:16:48 +0545

    Making this class used to change the x and y position of the object when clicked on gizmos either on z-axis or y-axis.

---
+++ src/main/java/components/TranslateGizmo.java
@@ -0,0 +1,28 @@
+package components;
+
+import editor.PropertiesWindow;
+import engine.MouseListener;
+
+// This class is used to change the x and y position of the object when clicked on gizmos either on x axis or y axis
+// gizmos are the arrows that appears when clicked on the object
+
+public class TranslateGizmo extends Gizmo{
+
+    public TranslateGizmo(Sprite arrowSprite, PropertiesWindow propertiesWindow){
+        super(arrowSprite, propertiesWindow);
+
+    }
+
+    // checks which arrows is active if it is x then converts it into the world space
+    @Override
+    public void editorUpdate(float dt) {
+        if (activeGameObject != null ){
+            if (xAxisActive && !yAxisActive){
+                activeGameObject.transform.position.x -= MouseListener.getScreenX();
+            } else if (yAxisActive){
+                activeGameObject.transform.position.y -= MouseListener.getScreenY();
+            }
+        }
+        super.editorUpdate(dt);
+    }
+}
